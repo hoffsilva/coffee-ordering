ab6def021cac02ad32f937a748d3502f3d6f022f
Hoff Silva <hoff.henry@gmail.com>
2021-03-08 20:20:56 -0300

    Creating project

---
+++ Coffee ordering/Coffee ordering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coffee ordering
+//
+//  Created by Hoff Henry Pereira da Silva on 08/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Coffee ordering/Coffee orderingTests/Coffee_orderingTests.swift
@@ -0,0 +1,33 @@
+//
+//  Coffee_orderingTests.swift
+//  Coffee orderingTests
+//
+//  Created by Hoff Henry Pereira da Silva on 08/03/21.
+//
+
+import XCTest
+@testable import Coffee_ordering
+
+class Coffee_orderingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
